213498b2bfedc110280f11cc1ff2da35ed691544
kd2code <das.krishna94@gmail.com>
2021-10-28 20:48:57 +0530

    updated commit

--- CollectionsDemo.java
+++ CollectionsDemo.java
@@ -42,14 +42,14 @@ public class CollectionsDemo {
 		Employee e3=new Employee(15,"Sita");
 		List al=new ArrayList<>();
 		Sortid sid=new Sortid();
-		Collections.addAll(al,e1,e2,e3);
-		//Collections.addAll(al,"Ram","Sita","Shyam","Lakshman");
+		//Collections.addAll(al,e1,e2,e3);
+		Collections.addAll(al,"Ram","Sita","Shyam","Lakshman");
 		System.out.println("Created ArrayList:"+al);
-	    Collections.sort(al,sid);	
+	    Collections.sort(al);	
 	    System.out.println("After sorting:"+al);
-	    System.out.println("Searching:"+Collections.binarySearch(al,e3,sid));
-	    Collections.sort(al,Collections.reverseOrder(sid));
-	    System.out.println("Reverse Sorted :"+al);
+	    //System.out.println("Searching:"+Collections.binarySearch(al,e3,sid));
+	    //Collections.sort(al,Collections.reverseOrder(sid));
+	    //System.out.println("Reverse Sorted :"+al);
 	    
 	}
 }
